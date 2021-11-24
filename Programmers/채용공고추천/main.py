e2b60d838aa121a3c4e40a72e43e10cb8f1d3d16
alsrua7222 <59680587+alsrua7222@users.noreply.github.com>
2021-11-24 13:42:11 +0900

    Update main.py

--- Programmers/채용공고추천/main.py
+++ Programmers/채용공고추천/main.py
@@ -2,34 +2,45 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
 import seaborn
 from Data import *
 
-if not os.path.isfile(r"C:\Users\KMK\Desktop\train_job\train2.csv"):
-    trains = pd.read_csv(r"C:\Users\KMK\Desktop\train_job\train.csv")
-    tags = pd.read_csv(r"C:\Users\KMK\Desktop\train_job\tags.csv")
-    job_tags = pd.read_csv(r"C:\Users\KMK\Desktop\train_job\job_tags.csv")
-    user_tags = pd.read_csv(r"C:\Users\KMK\Desktop\train_job\user_tags.csv")
+path = r"C:\Users\KMK\Desktop\train_job"
+if not os.path.isfile(path + r"\train2.csv"):
+    trains = pd.read_csv(path + r"\train.csv")
+    tags = pd.read_csv(path + r"\tags.csv")
+    job_tags = pd.read_csv(path + r"\job_tags.csv")
+    user_tags = pd.read_csv(path + r"\user_tags.csv")
     trains = getUserTagRatio(trains, job_tags, user_tags)
     if trains is None:
         print("getUserTagRatio return => None")
         exit(0)
     else:
-        trains.to_csv(r"C:\Users\KMK\Desktop\train_job\train2.csv", index=False, sep=',')
+        trains.to_csv(path + r"\train2.csv", index=False, sep=',')
 else:
-    trains = pd.read_csv(r"C:\Users\KMK\Desktop\train_job\train2.csv")
+    trains = pd.read_csv(path + r"\train2.csv")
 
-if not os.path.isfile(r"C:\Users\KMK\Desktop\train_job\train3.csv"):
-    job_companies = pd.read_csv(r"C:\Users\KMK\Desktop\train_job\job_companies.csv")
+if not os.path.isfile(path + r"\train3.csv"):
+    job_companies = pd.read_csv(path + r"\job_companies.csv")
     trains = getJobCompanySize(trains, job_companies)
     if trains is None:
         print("getJobCompanySize return => None")
         exit(0)
     else:
-        trains.to_csv(r"C:\Users\KMK\Desktop\train_job\train3.csv", index=False, sep=',')
+        trains.to_csv(path + r"\train3.csv", index=False, sep=',')
 else:
-    trains = pd.read_csv(r"C:\Users\KMK\Desktop\train_job\train3.csv")
+    trains = pd.read_csv(path + r"\train3.csv")
+
+if not os.path.isfile(path + r"t\rain4.csv"):
+    user_tags = pd.read_csv(path + r"\user_tags.csv")
+    trains = getUserTagCounts(trains, user_tags)
+    if trains is None:
+        print("getUserTagCounts return => None")
+        exit(0)
+    else:
+        trains.to_csv(path + r"\train4.csv", index=False, sep=",")
+else:
+    trains = pd.read_csv(path + r"train4.csv")
 
 # companySize에 따라 지원 횟수가 늘어나는지 검증해본다.
 # x축은 NaN, 1-10, 11-50, 51-100, 101-200 인지 체크해본다.
@@ -37,5 +48,64 @@ else:
 
 # seaborn.stripplot(x="companySize", y="UserTagRatio", hue="applied", data=trains)
 # 선형 회귀 분석 시도.
-seaborn.lmplot(x="companySize", y="UserTagRatio", hue="applied", data=trains)
+# seaborn.lmplot(x="companySize", y="UserTagRatio", hue="applied", data=trains)
+
+# UserTagRatio와 UserTagCounts가 서로 선형 관계인지 본다.
+# seaborn.lmplot(y="UserTagRatio", x="UserTagCounts", hue="applied", data=trains)
+
+# 3차원 lm분석
+# seaborn.lmplot(y="UserTagRatio", x="UserTagCounts", hue="applied", col="companySize", data=trains)
+# plt.show()
+
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.losses import binary_crossentropy
+
+x_train, x_test, y_train, y_test = train_test_split(trains[['UserTagCounts', 'UserTagRatio']], trains[['applied']], test_size=0.2, random_state=123)
+
+mlp = Sequential()
+mlp.add(Dense(2, input_shape=(2, 1)))
+# mlp.add(Dense(3))
+mlp.add(Dense(1, activation="sigmoid"))
+
+mlp.summary()
+
+# default - learning_rate = 0.01
+sgd = SGD()
+
+mlp.compile(optimizer=sgd, loss=binary_crossentropy, metrics=['accuracy'])
+
+history = mlp.fit(x_train, y_train, batch_size=1, epochs=20, validation_split=0.2, verbose=2)
+
+plt.plot(history.history['loss'], 'b-', label='loss')
+plt.title("Model loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
 plt.show()
+
+plt.plot(history.history['accuracy'], 'g-', label='accuracy')
+plt.title("accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("accuracy")
+plt.legend()
+plt.show()
+
+# 손실 함수 계산
+
+mlp.evaluate(x_test, y_test, batch_size=1, verbose=2)
+line_x = np.arange(min(x_test), max(x_test), 0.01)
+line_y = mlp.predict(line_x)
+
+plt.plot(line_x, line_y, 'r-')
+plt.plot(x_test, y_test, 'bo')
+plt.title("Model")
+plt.xlabel("test")
+plt.ylabel("predict")
+plt.legend(['predict', 'test'])
+plt.show()
+
+
